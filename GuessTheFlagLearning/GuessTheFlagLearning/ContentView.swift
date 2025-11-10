a57f4520158e30d8e6ddff29185aa65f52ecd139
Andrew Micenko <andrew.micenko@gmail.com>
2025-11-10 11:17:06 +0800

    Beginnings of "guess the flag" project.

---
+++ GuessTheFlagLearning/GuessTheFlagLearning/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  GuessTheFlagLearning
+//
+//  Created by Andrew Micenko on 10/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.primary.opacity(0.25)
+
+            // Shift-Command-A flips light/dark mode in simulator
+            VStack(spacing: 0) {
+                Color.green
+                Color.yellow
+            }
+
+            //            LinearGradient(
+            //                stops: [
+            //                    Gradient.Stop(color: .yellow, location: 0.35),
+            //                    .init(color: .green, location: 0.65),
+            //                ],
+            //                startPoint: .topLeading,
+            //                endPoint: .bottomTrailing
+            //            )
+
+            //            RadialGradient(colors: [.cyan, .mint], center: .center, startRadius: 20, endRadius: 180)
+
+            //            AngularGradient(
+            //                colors: [.red, .yellow, .green, .blue, .purple, .red],
+            //                center: .init(x: 0.25, y: 1.0 / 3)
+            //            )
+
+            // Color.orange.frame(minWidth: 200, maxWidth: 300, maxHeight: 200)
+
+            Text("This was funny").frame(
+                minWidth: 200,
+                maxWidth: 300,
+                maxHeight: 200,
+                alignment: .top
+            )
+            .background(.indigo.gradient)
+            .cornerRadius(10)
+
+            Text("Hi Everybody!")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+                //.background(.red.gradient // Subtle OS-controlled gradient
+                .cornerRadius(20)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlagLearning/GuessTheFlagLearning/GuessTheFlagLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagLearningApp.swift
+//  GuessTheFlagLearning
+//
+//  Created by Andrew Micenko on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
