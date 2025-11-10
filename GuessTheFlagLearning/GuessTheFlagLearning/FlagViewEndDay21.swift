5a4c12c03514d424aeaf8cc49e475146cac419e7
Andrew Micenko <andrew.micenko@gmail.com>
2025-11-10 15:04:21 +0800

    Flag guessing game - end of day 21.

---
+++ GuessTheFlagLearning/GuessTheFlagLearning/FlagViewEndDay21.swift
@@ -0,0 +1,106 @@
+//
+//  FlagView.swift
+//  GuessTheFlagLearning
+//
+//  Created by Andrew Micenko on 10/11/2025.
+//
+
+import SwiftUI
+
+private var countries = [
+    "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria",
+    "Poland", "Spain", "UK", "Ukraine", "USA",
+].shuffled()
+
+struct FlagViewEndDay21: View {
+    @State var currentCorrectAnswer = Int.random(in: 0..<4)
+
+    @State private var isScoreShowing = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+
+    var body: some View {
+        ZStack {
+            RadialGradient(
+                stops: [
+                    .init(color: .green, location: 0.3),
+                    .init(color: .yellow, location: 0.3),
+                ],
+                center: .top,
+                startRadius: 200,
+                endRadius: 700
+            ).ignoresSafeArea()
+
+            VStack {
+                Spacer()
+
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of").padding(15).font(
+                            .subheadline.weight(.heavy)
+                        ).foregroundStyle(.secondary)
+                        Text(countries[currentCorrectAnswer]).padding()
+                            .foregroundStyle(
+                                .selection
+                            ).font(.largeTitle.weight(.semibold))
+
+                    }.background(.thinMaterial)
+                        .cornerRadius(20)
+
+                    ForEach(0..<4, id: \.self) { number in
+                        Button {
+                            onFlagTapped(number)
+                        } label: {
+                            Image("Flags/\(countries[number])").clipShape(
+                                .capsule
+                            )
+                        }.shadow(radius: 7)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+
+                Spacer()
+                Spacer()
+
+                Text("Score: \(score)")
+                    .font(.title.bold())
+
+                Spacer()
+            }
+            .padding()
+            .alert(scoreTitle, isPresented: $isScoreShowing) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(score)")
+            }
+        }
+    }
+
+    func onFlagTapped(_ number: Int) {
+        if number == currentCorrectAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Incorrect"
+            score = 0
+        }
+
+        isScoreShowing = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        currentCorrectAnswer = Int.random(in: 0..<4)
+    }
+}
+
+#Preview {
+    FlagViewEndDay21()
+}
--- GuessTheFlagLearning/GuessTheFlagLearning/GuessTheFlagLearningApp.swift
+++ GuessTheFlagLearning/GuessTheFlagLearning/GuessTheFlagLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GuessTheFlagLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            FlagView()
+            FlagViewEndDay21()
         }
     }
 }
